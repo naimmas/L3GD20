4fe368be23f6efbbd393b8c0a8ba253c69ed1cf2
naimmas <naeemmas@gmail.com>
2022-03-05 01:56:55 +0300

    fixing DEBUG_EN definition statements

--- L3GD20.c
+++ L3GD20.c
@@ -15,15 +15,22 @@ HAL_StatusTypeDef L3GD20_CheckDevice(L3GD20 *dev)
 		L3GD20_ReadRegister(dev, L3GD20_WHO_AM_I, &id);
 	return (id==L3GD20_Device_ID) ? (HAL_OK) : (HAL_ERROR);
 }
-
+#ifdef DEBUG_EN
 L3GD20_ErrorTypeDef L3GD20_Init(L3GD20 *dev, I2C_HandleTypeDef *i2cHandle, uint8_t* errHandling)
+#else
+L3GD20_ErrorTypeDef L3GD20_Init(L3GD20 *dev, I2C_HandleTypeDef *i2cHandle)
+#endif
 {
 	dev->i2cHandle = i2cHandle;
+#ifdef DEBUG_EN
 	*errHandling = 0x0;
+#endif
 	uint8_t dataWr;
 	if(L3GD20_CheckDevice(dev) == HAL_ERROR) {
+#ifdef DEBUG_EN
 		*errHandling = 0xF;
-		return SENSOR_CONNECTION_ERROR;
+#endif
+		return L3GD20_SENSOR_CONNECTION_ERROR;
 	}
 	
 	dataWr = ODR380_CO100 | ENABLE_ALL_AXES | POWER_NORMAL_MODE;
@@ -42,7 +49,7 @@ L3GD20_ErrorTypeDef L3GD20_Init(L3GD20 *dev, I2C_HandleTypeDef *i2cHandle, uint8
 		}
 
 	#ifdef DEBUG_EN
-		return (*errHandling==0x0) ? REGISTER_WRITE_OK : REGISTER_WRITE_ERROR;
+		return (*errHandling==0x0) ? L3GD20_REGISTER_WRITE_OK : L3GD20_REGISTER_WRITE_ERROR;
 	#endif
 
 	#ifndef DEBUG_EN
@@ -61,9 +68,9 @@ HAL_StatusTypeDef L3GD20_ReadGyro(L3GD20 *dev)
 	state |= L3GD20_ReadRegister(dev, L3GD20_OUT_Z_H, &inComeData[5]);
 	if (state==HAL_OK)
 	{
-		tempData[0] = ((int16_t) inComeData[1]<<8 | inComeData[0]);
-		tempData[1] = ((int16_t) inComeData[3]<<8 | inComeData[2]);
-		tempData[2] = ((int16_t) inComeData[5]<<8 | inComeData[4]);
+		tempData[0] = (int16_t) (inComeData[1]<<8 | inComeData[0]);
+		tempData[1] = (int16_t) (inComeData[3]<<8 | inComeData[2]);
+		tempData[2] = (int16_t) (inComeData[5]<<8 | inComeData[4]);
 		dev->gyro[0]=(double) tempData[0] * L3GD20GyroSens * DPS_TO_RADS;
 		dev->gyro[1]=(double) tempData[1] * L3GD20GyroSens * DPS_TO_RADS;
 		dev->gyro[2]=(double) tempData[2] * L3GD20GyroSens * DPS_TO_RADS;
